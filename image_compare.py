6f58d338ad799bc6b57b0613ed648fb948419a23
Emily Wang <emilyluwang@college.harvard.edu>
2014-06-11 15:55:01 -0400

    playing around with image comparison tools

---
+++ image_compare.py
@@ -0,0 +1,11 @@
+
+import scipy 
+
+# now we obtain a difference image of the two screenshots
+#im1 = Image.open(str(img_filepath + link_name + '-clipped.png'))
+#im2 = Image.open(str(img_filepath + file_name + '-clipped.png'))
+#diff = ImageChops.difference(im2, im1)
+#diff.show()
+
+# calculate the minimum bounding box to quantify the difference image
+#print diff.getbbox()
--- screenshot_url.py
+++ screenshot_url.py
@@ -1,11 +1,15 @@
 import os
 import hashlib
 import httplib2
+from PIL import Image, ImageChops
 from bs4 import BeautifulSoup, SoupStrainer
 
 # the page containing all links to visit and screenshot
 host_url = 'http://ec2-75-101-154-48.compute-1.amazonaws.com/node/12'
 cache_url = 'http://ec2-75-101-154-48.compute-1.amazonaws.com/CAYL/cache/'
+img_filepath = '../../../Desktop/images_temp/'
+WIDTH = 400
+HEIGHT = 400
 
 # getting the source code of the page at host_url
 http = httplib2.Http()
@@ -17,17 +21,20 @@ status, response = http.request(host_url)
 for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
     if link.has_attr('href'):
         text_of_link = link['href']
+        
+        # if a link to an external website
         if 'http://' in text_of_link:
-            # figure out some way to calculate hash of link as well
-            #print text_of_link
+
+            # print text_of_link
             link_name = text_of_link[7:]
             if '/' in link_name:
+                #remove the last backslash
                 link_name = link_name[::-1][1:][::-1]
-            print link_name
-            os.system('webkit2png -F -D ~/Desktop/images_temp -o ' + link_name + ' ' + text_of_link)
+            os.system('webkit2png --clipwidth=' + str(WIDTH) + ' --clipheight= ' + str(HEIGHT) +' -D ~/Desktop/images_temp -o ' + link_name + ' ' + text_of_link)
+
             # now we need to calculate hash
             new_hash = hashlib.md5(text_of_link)
             hash_link = cache_url + new_hash.hexdigest() + '/'
             file_name = link_name + '_cache'
-            os.system('webkit2png -F -D ~/Desktop/images_temp -o ' + file_name + ' ' + hash_link)
+            os.system('webkit2png --clipwidth=' + str(WIDTH) + ' --clipheight=' + str(HEIGHT) + ' -D ~/Desktop/images_temp -o ' + file_name + ' ' + hash_link)
 
