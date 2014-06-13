8bc6c8b26d2b623bce9b31f8b3a6308f265ca0b6
Emily Wang <emilyluwang@college.harvard.edu>
2014-06-13 10:40:44 -0400

    fixed problems with beautiful soup parsing

--- screenshot_url.py
+++ screenshot_url.py
@@ -13,29 +13,32 @@ img_filepath = '../../../Desktop/images_temp/'
 http = httplib2.Http()
 status, response = http.request(host_url)
 
+soup = BeautifulSoup(response, "lxml")
+
 # filtering source code for only href tags
 # extracts links from href tags
 # then runs a commnand line process to screenshot all links
-for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
-    if link.has_key('href'):
-        text_of_link = link['href']
+for tag in soup.findAll('a', href = True):
+    text_of_link = str(tag.contents[0])
         
-        # if a link to an external website
-        if 'http://' in text_of_link:
+    # if a link to an external website
+    if 'http://' in text_of_link:
 
-            # print text_of_link
-            link_name = text_of_link[7:]
-            if '/' in link_name:
-                #remove the last backslash
-                link_name = link_name[::-1][1:][::-1]
-            #os.system('webkit2png --clipwidth=' + str(WIDTH) + ' --clipheight= ' + str(HEIGHT) +' -D ~/Desktop/images_temp -o ' + link_name + ' ' + text_of_link)
-            os.system('webkit2png -C -D ~/Desktop/images_temp -o ' + link_name + ' ' + text_of_link)
+        link_name = text_of_link[7:]
+        if '/' in link_name:
+    
+            #remove the last backslash
+            link_name = link_name[::-1][1:][::-1]
+    
+        #os.system('webkit2png --clipwidth=' + str(WIDTH) + ' --clipheight= ' + str(HEIGHT) +' -D ~/Desktop/images_temp -o ' + link_name + ' ' + text_of_link)
+        os.system('webkit2png -C -D ~/Desktop/images_temp -o ' + link_name + ' ' + text_of_link)
 
-            # now we need to calculate hash
-            new_hash = hashlib.md5(text_of_link)
-            hash_link = cache_url + new_hash.hexdigest() + '/'
-            file_name = link_name + '_cache'
-            #os.system('webkit2png --clipwidth=' + str(WIDTH) + ' --clipheight=' + str(HEIGHT) + ' -D ~/Desktop/images_temp -o ' + file_name + ' ' + hash_link)
-            os.system('webkit2png -C -D ~/Desktop/images_temp -o ' + file_name + ' ' + hash_link)
+        # now we need to calculate hash
+        new_hash = hashlib.md5(text_of_link)
+        hash_link = cache_url + new_hash.hexdigest() + '/'
+        file_name = link_name + '_cache'
+    
+        #os.system('webkit2png --clipwidth=' + str(WIDTH) + ' --clipheight=' + str(HEIGHT) + ' -D ~/Desktop/images_temp -o ' + file_name + ' ' + hash_link)
+        os.system('webkit2png -C -D ~/Desktop/images_temp -o ' + file_name + ' ' + hash_link)
 
 
--- test1.py
+++ test1.py
@@ -1,18 +1,20 @@
 import httplib2
+import lxml
 import urllib
 from bs4 import BeautifulSoup, SoupStrainer
-from BeautifulSoup import BeautifulStoneSoup
 
 host_url = 'http://ec2-75-101-154-48.compute-1.amazonaws.com/node/12'
 
-#http = httplib2.Http()
-#status, response = http.request(host_url)
+http = httplib2.Http()
+status, response = http.request(host_url)
 
-f = urllib.urlopen(host_url)
-s = f.read()
-f.close()
+soup = BeautifulSoup(response, "lxml")
 
-soup = BeautifulStoneSoup(s)
+#f = urllib.urlopen(host_url)
+#s = f.read()
+#f.close()
+
+#soup = BeautifulSoup(s)
 
 
 #for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
@@ -22,7 +24,19 @@ soup = BeautifulStoneSoup(s)
         #print link
 	#print link.contents
 
-stuff_to_parse = soup.findAll("a")
-for each_link in stuff_to_parse:
-    link = stuff_to_parse["href"]
-    print link
+for tag in soup.findAll('a', href = True):
+	link_name = str(tag.contents[0])
+
+	if 'http://' in link_name:
+		print link_name
+#for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
+	#print link
+	
+
+
+
+
+#stuff_to_parse = soup.findAll("a")
+#for each_link in stuff_to_parse:
+    #link = stuff_to_parse['href']
+    #print link
